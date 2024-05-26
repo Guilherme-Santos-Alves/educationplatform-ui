ffe65ac458ea68f693abf7ba7ae22220f39dacbf
Guilherme <guilhermesantosalves068@gmail.com>
2024-05-25 21:23:51 -0300

    criação da home de aulas iniciada

--- js/index.js
+++
@@ -1,5 +0,0 @@
-function moverCard(){
-    const container = document.querySelector('.costumer-reviews-content');
-    const card = document.querySelector('.card-review');
-    container.appendChild(card);
-}
