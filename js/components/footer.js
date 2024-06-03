4a926e2dc853180d5ec5155c65a856978ac55824
Guilherme <guilhermesantosalves068@gmail.com>
2024-06-03 20:02:37 -0300

    Sidebar da home de videos finalizada

---
+++ js/components/footer.js
@@ -0,0 +1,75 @@
+class Footer extends HTMLElement{
+    connectedCallback() {
+        this.innerHTML = `
+            <footer class="ep-footer">
+                <div id="footer-content">
+                    <div id="footer-contacts">
+                        <h1>logo</h1>
+                        <p>it's all about your dream</p>
+                        <div id="footer-social-media">
+                            <a href="" class="footer-link" id="instagram">
+                                <i class="fa-brands fa-instagram"></i>
+                            </a>
+
+                            <a href="" class="footer-link" id="facebook">
+                                <i class="fa-brands fa-facebook"></i>
+                            </a>
+                            <a href="" class="footer-link" id="whatsapp">
+                                <i class="fa-brands fa-whatsapp"></i>
+                            </a>
+                        </div>
+                    </div>
+
+                    <ul class="footer-list">
+                        <li>
+                            <h3>Blog</h3>
+                        </li>
+                        <li>
+                            <a href="https://www.linkedin.com/in/guilherme-santos-alves-50088b1a8/" target="_blank" class="footer-link">Linkedin</a>
+                        </li>
+                        <li>
+                            <a href="https://github.com/Guilherme-Santos-Alves" target="_blank" class="footer-link">GitHub</a>
+                        </li>
+                        <li>
+                            <a href="" class="footer-link">Music</a>
+                        </li>
+                    </ul>
+
+                    <ul class="footer-list">
+                        <li>
+                            <h3>Products</h3>
+                        </li>
+                        <li>
+                            <a href="" class="footer-link">App</a>
+                        </li>
+                        <li>
+                            <a href="" class="footer-link">Desktop</a>
+                        </li>
+                        <li>
+                            <a href="" class="footer-link">Cloud</a>
+                        </li>
+                    </ul>
+
+                    <div id="footer-subscribe">
+                        <h3>Inscrever-se</h3>
+                        <p>
+                            Fique por dentro das últimas novidades, promoções exclusivas e conteúdo especial diretamente no seu e-mail.
+                        </p>
+                        
+                        <div id="input-group">
+                            <input type="email" name="" id="subscriber-email">
+                            <button>
+                                <i class="fa-regular fa-envelope"></i>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <div id="footer-copyright">
+                    &#169
+                    2024 all rights reserved - Developed by Guilherme Santos Alves
+                </div>
+            </footer>`;
+    }
+}
+
+customElements.define('ep-footer', Footer);
---
+++ js/components/sidebar-home.js
@@ -0,0 +1,58 @@
+class Nav extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+            <div class="ep-sidebar-content">
+                <ul id="side-itens">
+                    <li class="side-item active">
+                        <a href="">
+                            <i class="fa-solid fa-house"></i>
+                            <span class="item-description">
+                                Início
+                            </span>
+                        </a>
+                    </li>
+                    <li class="side-item">
+                        <a href="">
+                            <i class="fa-regular fa-user"></i>
+                            <span class="item-description">
+                                Perfil
+                            </span>
+                        </a>
+                    </li>
+                    <li class="side-item">
+                        <a href="">
+                            <i class="fa-regular fa-message"></i>
+                            <span class="item-description">
+                                Suporte
+                            </span>
+                        </a>
+                    </li>
+                    <li class="side-item">
+                        <a href="">
+                            <i class="fa-solid fa-medal"></i>
+                            <span class="item-description">
+                                Certificados
+                            </span>
+                        </a>
+                    </li>
+                    <button id="logout-btn">
+                        <i class="fa-solid fa-arrow-right-from-bracket"></i>
+                        <span class="item-description">
+                            Sair
+                        </span>
+                    </button>
+                </ul>
+                <button id="open-sidebar-btn">
+                    <i id="open-sidebar-icon" class="fa-solid fa-chevron-right"></i>
+                </button>
+            </div>`;
+    }
+}
+
+customElements.define('ep-sidebar-home', Nav);
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('open-sidebar-btn').addEventListener('click', () => {
+        document.getElementById('ep-sidebar').classList.toggle('open-ep-sidebar');
+    })
+})
