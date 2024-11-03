0d7389c0b66eca1f083b56b40ba8c0bc8626d2bf
Guilherme <guilhermesantosalves068@gmail.com>
2024-11-03 19:18:40 -0300

    Script de habilitar input adicionado

---
+++ js/course-update/course-update.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const editButtons = document.querySelectorAll('.edit');
+
+    editButtons.forEach(button => {
+        button.addEventListener('click', function() {
+            const parentWithCsName  = button.closest('.name');
+            const parentWithCsDesc  = button.closest('.description');
+            const parentWithCsCover = button.closest('.cover');
+            if (parentWithCsName) {
+                const inputName = parentWithCsName.querySelector('#cs-name');
+                inputName.disabled = !inputName.disabled;
+            } else if (parentWithCsDesc) {
+                const inputDesc = parentWithCsDesc.querySelector('#cs-desc');
+                inputDesc.disabled = !inputDesc.disabled;
+            } else if (parentWithCsCover) {
+                const inputCover = parentWithCsCover.querySelector('#cs-cover');
+                inputCover.disabled = !inputCover.disabled;
+            }
+
+            const parentWithMdName = button.closest('.module');
+            if (parentWithMdName) {
+                const inputMdName = parentWithMdName.querySelector('.md-name');
+                inputMdName.disabled = !inputMdName.disabled;
+            }
+
+            const parentWithLsName = button.closest('.lesson');
+            if (parentWithLsName) {
+                const inputLsName = parentWithLsName.querySelector('.ls-name');
+                inputLsName.disabled = !inputLsName.disabled;
+            }
+        });
+    });
+});
