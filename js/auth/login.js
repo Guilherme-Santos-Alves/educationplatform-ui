374926d32aa229840bc7b3634856f83db762e9f8
Guilherme <guilhermesantosalves068@gmail.com>
2024-12-21 14:37:48 -0300

    Validacao de login adicionado

---
+++ js/auth/login.js
@@ -0,0 +1,51 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const formLogin = document.querySelector('#form-login');
+    const email = document.querySelector('#email');
+    const password = document.querySelector('#password');
+
+    formLogin.addEventListener('submit', (e)=> {
+        e.preventDefault();
+
+        const formData = {
+            email: email.value,
+            password: password.value
+        };
+
+        login(formData);
+    });
+
+    function login(formData) {
+        fetch(`https://localhost:7092/api/auth`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(formData)
+        })
+        .then( response => {
+            console.log(response);
+            if(response.ok){
+                response.json().then(( data ) => {
+                    localStorage.setItem('tokenJwt', data.data.token);
+
+                    Swal.fire({
+                        text: "Login realizado com sucesso!",
+                        icon: "success"
+                    });
+
+                    setTimeout(() => {
+                        window.location.href = 'main-home.html';
+                    }, 500);
+                });
+            } else{
+                response.text().then(( errorMsg ) => {
+                    const passwordError = document.getElementById("login-error");
+                    passwordError.classList.remove("hidden");
+                    passwordError.classList.add("visible");
+                
+                    passwordError.textContent = errorMsg || "Login inválido.";
+                });
+            }
+        });
+    }
+})
