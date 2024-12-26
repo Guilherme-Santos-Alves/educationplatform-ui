0d07b918a45b36ea0fa5ebd8bb4556c16d739242
Guilherme <guilhermesantosalves068@gmail.com>
2024-12-26 20:30:01 -0300

    Tela de módulos corrigida

--- js/components/sidebar-home.js
+++ js/components/sidebar-home.js
@@ -1,6 +1,9 @@
 class SidebarHome extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
             <nav id="ep-sidebar" class="sidebar p-3">
                 <ul class="nav flex-column">
                     <li class="nav-item mb-2">
---
+++ js/course-modules/course-modules.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const modulos = document.querySelectorAll('.module');
+    modulos.forEach((modulo) => {
+        const openModule = modulo.querySelector('.module-header');
+        if (openModule) {
+            openModule.addEventListener('click', () => {
+                modulo.classList.toggle('-hide');
+            });
+        }
+    });
+});
