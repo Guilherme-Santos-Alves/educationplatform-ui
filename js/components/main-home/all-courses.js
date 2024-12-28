a63a3498c351572e29789e1338272ab663b55bd2
Guilherme <guilhermesantosalves068@gmail.com>
2024-12-28 20:03:28 -0300

    Cursos repetidos deletados

--- js/components/main-home/all-courses.js
+++ js/components/main-home/all-courses.js
@@ -14,61 +14,6 @@ class AllCoursesSection extends HTMLElement {
                             <button>Ver curso</button>
                         </div>
                     </div>
-                    <div class="course">
-                        <div class="course-picture">
-                            <img src="https://miro.medium.com/v2/resize:fit:1358/0*VYGU5sx8ET8hbar1" alt="">
-                        </div>
-                        <div class="course-title">
-                            <h1>Curso</h1>
-                        </div>
-                        <div class="course-btn">
-                            <button>Ver curso</button>
-                        </div>
-                    </div>
-                    <div class="course">
-                        <div class="course-picture">
-                            <img src="https://miro.medium.com/v2/resize:fit:1358/0*VYGU5sx8ET8hbar1" alt="">
-                        </div>
-                        <div class="course-title">
-                            <h1>Curso</h1>
-                        </div>
-                        <div class="course-btn">
-                            <button>Ver curso</button>
-                        </div>
-                    </div>
-                    <div class="course">
-                        <div class="course-picture">
-                            <img src="https://miro.medium.com/v2/resize:fit:1358/0*VYGU5sx8ET8hbar1" alt="">
-                        </div>
-                        <div class="course-title">
-                            <h1>Curso</h1>
-                        </div>
-                        <div class="course-btn">
-                            <button>Ver curso</button>
-                        </div>
-                    </div>
-                    <div class="course">
-                        <div class="course-picture">
-                            <img src="https://miro.medium.com/v2/resize:fit:1358/0*VYGU5sx8ET8hbar1" alt="">
-                        </div>
-                        <div class="course-title">
-                            <h1>Curso</h1>
-                        </div>
-                        <div class="course-btn">
-                            <button>Ver curso</button>
-                        </div>
-                    </div>
-                    <div class="course">
-                        <div class="course-picture">
-                            <img src="https://miro.medium.com/v2/resize:fit:1358/0*VYGU5sx8ET8hbar1" alt="">
-                        </div>
-                        <div class="course-title">
-                            <h1>Curso</h1>
-                        </div>
-                        <div class="course-btn">
-                            <button>Ver curso</button>
-                        </div>
-                    </div>
                 </div>
             </div>`;
     }
