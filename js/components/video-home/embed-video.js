f31233a7d5721fbaf3e545770a9043d3d471b672
Guilherme <guilhermesantosalves068@gmail.com>
2025-01-01 22:04:39 -0300

    Aulas do mesmo módulo adicionadas ao lado do video

--- js/components/video-home/embed-video.js
+++ js/components/video-home/embed-video.js
@@ -1,20 +1,9 @@
-function extractVimeoId(url) {
-    if (!url) {
-        console.warn("URL inválida ou ausente.");
-        return null;
-    }
-    const regex = /vimeo\.com\/(\d+)/;
-    const match = url.match(regex);
-    return match ? match[1] : null;
-}
-
 function getQueryParam(param) {
     const urlParams = new URLSearchParams(window.location.search);
     return urlParams.get(param);
 }
 
-const lessonUrl = getQueryParam('lessonUrl');
-const lessonId = extractVimeoId(lessonUrl);
+const lessonId = getQueryParam('lessonId');
 
 class EmbedVideo extends HTMLElement {
     connectedCallback() {
@@ -30,8 +19,8 @@ class EmbedVideo extends HTMLElement {
         this.innerHTML = `
             <iframe class="video" 
                 src="https://player.vimeo.com/video/${lessonId}?badge=0&amp;autopause=0&amp;transparent=0&amp;player_id=0&amp;app_id=58479" 
-                frameborder="0" 
-                allow="autoplay; fullscreen; picture-in-picture; clipboard-write" 
+                    frameborder="0" 
+                allow="autoplay; fullscreen; picture-in-picture" 
                 title="Video Player">
             </iframe>
         `;
---
+++ js/video-home/get-lesson-of-module.js
@@ -0,0 +1,51 @@
+function getQueryParam(param) {
+    const urlParams = new URLSearchParams(window.location.search);
+    return urlParams.get(param);
+}
+
+const moduleId = getQueryParam('moduleId');
+
+document.addEventListener('DOMContentLoaded', getLessonsOfModule);
+
+function getLessonsOfModule() {
+    fetch(`https://localhost:7092/api/videolessons/module/${moduleId}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${tokenJwt}`
+        },
+    })
+    .then(response => {
+        if (!response.ok) {
+            return response.text().then(errorText => {
+                throw new Error(errorText);
+            });
+        }
+        return response.json();
+    })
+    .then(lessons => {
+        lessons.data.forEach(lesson => {
+            const lessonContainer = document.querySelector('.module-lesson-list');
+
+            const lessonBody = `
+                <a href="video-home.html?lessonId=${lesson.vimeoVideoId}&moduleId=${moduleId}" class="module-lesson">
+                    <iframe class="lesson-thumb"
+                        src="https://player.vimeo.com/video/${lesson.vimeoVideoId}?badge=0&autopause=0&transparent=0&controls=0&autoplay=0&loop=0&muted=0&byline=0&title=0&player_id=0&app_id=58479" 
+                        frameborder="0" 
+                        allow="autoplay; fullscreen; picture-in-picture" 
+                        title="Video Player">
+                    </iframe>
+                    
+                    <h2 class="lesson-title">${lesson.name}</h2>
+                </a>`;
+
+            lessonContainer.insertAdjacentHTML('beforeend', lessonBody);
+        });
+    })
+    .catch(error => {
+        const lessonContainer = document.querySelector('.module-lesson-list');
+        const errorMsg = `<p class="error-message">${'Erro ao carregar a aula: ' + error.message}</p>`;
+
+       lessonContainer.insertAdjacentHTML('beforeend', errorMsg);
+    });
+}
